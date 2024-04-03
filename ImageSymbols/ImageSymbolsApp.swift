522d43ecee4a94be220e227484a1eb5e8a4caccd
Luis Bustamante <luisebt995@gmail.com>
2024-04-03 15:39:37 +0200

    Initial Commit

---
+++ ImageSymbols/ImageSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSymbolsApp.swift
+//  ImageSymbols
+//
+//  Created by Luis on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
