2c548db6f09de271e69efed3c5bf7df81f6a598b
Luis Bustamante <luisebt995@gmail.com>
2024-04-03 16:21:04 +0200

    Add a system symbol while adding animations and modifying its properties.

--- ImageSymbols/ContentView.swift
+++ ImageSymbols/ContentView.swift
@@ -5,12 +5,27 @@
 //  Created by Luis on 3/4/24.
 //
 
+//This project need SFSymbols 5.1
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorChange = false
+    @State private var sizeChange = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "heart.fill")
+            .font(.system(size: 200))
+            .foregroundColor(colorChange ? .yellow : .red) //Operador Ternario
+            .scaleEffect(sizeChange ? 1.5 : 1) //Operador Ternario
+            //.animation(.default)
+            .onTapGesture {
+                self.colorChange.toggle()
+            }
+            .onLongPressGesture {
+                self.sizeChange.toggle()
+            }
+        
     }
 }
 
